1e1e32b09dca1c57c1123b6ab27b7ad4c9c645d2
marmelad52 <aleks.skripoff2@gmail.com>
2024-11-21 22:28:54 +0300

    save

--- lib/src/websocket/classes.dart
+++ lib/src/websocket/classes.dart
@@ -442,7 +442,9 @@ class OrderUpdate {
         isCloseAll = m['cp'] ?? false,
         activationPrice = double.parse(m['AP'] ?? "0"),
         callbackRate = double.parse(m['cr'] ?? "0"),
-        realizedProfit = double.parse(m['rp']);
+        realizedProfit = double.parse(m['rp']) {
+    print(m.toString());
+  }
 }
 
 class WsOrderUpdateEvent {
